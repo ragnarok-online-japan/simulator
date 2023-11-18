9d2def916840f573c5d58042cb4a60b2032d3119
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2023-11-18 09:47:34 +0900

    [modify] Update files

--- app_v1.py
+++ app_v1.py
@@ -1,13 +1,20 @@
 # app.py
+import base64
 import binascii
 import bz2
+from io import BytesIO
 import json
 import traceback
 import urllib.parse
 from pyscript import document
-from js import location,URLSearchParams,XMLHttpRequest
+from js import location,URLSearchParams
+from PIL import Image, ImageFont, ImageDraw
+import requests
+import pyodide_http
+pyodide_http.patch_all()
 
 import package
+
 
 class Simulator:
     _initialized: bool = False
@@ -16,9 +23,76 @@ class Simulator:
     _suffix_url: str = "index.html"
 
     dom_elements: dict[str] = {}
+    load_datas: dict[str] = {}
 
-    _status_basic: tuple = ("str", "agi", "vit", "int", "dex", "luk")
-    _status_special: tuple = ("pow", "sta", "wis", "spl", "con", "crt")
+    _status_bases: dict = {
+        "str": {
+            "status_window_position" : (36, 6)
+        },
+        "agi": {
+            "status_window_position" : (36, 22)
+        },
+        "vit": {
+            "status_window_position" : (36, 38)
+        },
+        "int": {
+            "status_window_position" : (36, 54)
+        },
+        "dex": {
+            "status_window_position" : (36, 70)
+        },
+        "luk": {
+            "status_window_position" : (36, 86)
+        }
+    }
+    _status_specials: dict = {
+        "pow": {
+            "status_window_position" : (36, 128)
+        },
+        "sta": {
+            "status_window_position" : (36, 144)
+        },
+        "wis": {
+            "status_window_position" : (36, 160)
+        },
+        "spl": {
+            "status_window_position" : (36, 176)
+        },
+        "con": {
+            "status_window_position" : (36, 192)
+        },
+        "crt": {
+            "status_window_position" : (36, 208)
+        }
+    }
+
+    _status_result: dict = {
+        "hp_max": {
+            "status_window_position" : (0, 0)
+        },
+        "hp_recovery": {
+        },
+        "sp_max": {
+            "status_window_position" : (0, 0)
+        },
+        "sp_recovery": {
+        },
+        "hit": {
+            "status_window_position" : (0, 0)
+        },
+        "flee": {
+            "status_window_position" : (0, 0)
+        },
+        "complete_avoidance": {
+            "status_window_position" : (0, 0)
+        },
+        "critical": {
+            "status_window_position" : (0, 0)
+        },
+        "aspd": {
+            "status_window_position" : (0, 0)
+        }
+    }
 
     def __init__(self, prefix_url = "/", suffix_url="index.html") -> None:
         self._prefix_url = prefix_url
@@ -33,28 +107,28 @@ class Simulator:
         self.dom_elements["job_class"] = document.getElementById("status_job_class")
         self.dom_elements["job_class"].oninput = self.calculation
 
-        for key in self._status_basic:
+        for key in self._status_bases.keys():
             self.dom_elements[key]: dict = {
-                "basic"  : document.getElementById(f"status_{key}_basic"),
+                "base"  : document.getElementById(f"status_{key}_base"),
                 "bonus" : document.getElementById(f"status_{key}_bonus"),
             }
-            self.dom_elements[key]["basic"].oninput = self.calculation
+            self.dom_elements[key]["base"].oninput = self.calculation
 
         # 特性ステータス
-        for key in self._status_special:
+        for key in self._status_specials.keys():
             self.dom_elements[key]: dict = {
-                "basic"  : document.getElementById(f"status_{key}_basic"),
+                "base"  : document.getElementById(f"status_{key}_base"),
                 "bonus" : document.getElementById(f"status_{key}_bonus"),
             }
-            self.dom_elements[key]["basic"].oninput = self.calculation
+            self.dom_elements[key]["base"].oninput = self.calculation
 
         for key in ("atk", "def", "matk", "mdef"):
             self.dom_elements[key]: dict = {
-                "basic"  : document.getElementById(f"status_{key}_basic"),
+                "base"  : document.getElementById(f"status_{key}_base"),
                 "bonus" : document.getElementById(f"status_{key}_bonus"),
             }
 
-        for key in ("hp_max", "sp_max", "hit", "flee", "complete_avoidance", "critical", "aspd", "hp_recovery", "sp_recovery"):
+        for key in self._status_result.keys():
             self.dom_elements[key] = document.getElementById(f"status_{key}")
 
         self.dom_elements["button_import_json"] = document.getElementById("button_import_json")
@@ -65,17 +139,19 @@ class Simulator:
         self.dom_elements["daialog_button_close"] = document.getElementById("daialog_button_close")
         self.dom_elements["daialog_button_close"].onclick = self.close_dialog
 
-        xhr = XMLHttpRequest.new()
-        xhr.open("GET", prefix_url + "data/job_classes.json", False)
-        xhr.setRequestHeader("Content-Type", "application/json")
-        xhr.send()
-        self.data_job_classes = json.loads(xhr.response)
+        self.dom_elements["img_status_window"] = document.getElementById("img_status_window")
 
-        if len(self.data_job_classes) > 0:
+        self.dom_elements["button_draw_status_window"] = document.getElementById("button_draw_status_window")
+        self.dom_elements["button_draw_status_window"].onclick = self.onclick_draw_status_window
+
+        response = requests.get(prefix_url + "data/job_classes.json")
+        self.load_datas["job_classes"] = response.json()
+
+        if len(self.load_datas["job_classes"]) > 0:
             datalist_job_classes = document.getElementById("datalist_job_classes")
-            for idx in self.data_job_classes:
+            for idx in self.load_datas["job_classes"]:
                 child_class = document.createElement("option")
-                data = self.data_job_classes[idx]
+                data = self.load_datas["job_classes"][idx]
 
                 child_class.value = data["class"]
                 if "display_name" in data:
@@ -84,6 +160,36 @@ class Simulator:
                 datalist_job_classes.appendChild(child_class)
 
             self.dom_elements["job_class"].value = "novice"
+
+
+        response = requests.get(prefix_url + "data/weapon_types.json")
+        self.load_datas["weapon_types"] = response.json()
+
+        if len(self.load_datas["weapon_types"]) > 0:
+            # Right : Main weapon type
+            self.dom_elements["select_weapon_type_right"] = document.getElementById("select_weapon_type_right")
+            for idx in self.load_datas["weapon_types"]:
+                child_class = document.createElement("option")
+                data = self.load_datas["weapon_types"][idx]
+
+                child_class.value = data["name"]
+                if "display_name" in data:
+                    child_class.label = data["display_name"]
+
+                self.dom_elements["select_weapon_type_right"].appendChild(child_class)
+
+            # Left : Sub weapon type
+            self.dom_elements["select_weapon_type_left"]  = document.getElementById("select_weapon_type_left")
+            for idx in self.load_datas["weapon_types"]:
+                child_class = document.createElement("option")
+                data = self.load_datas["weapon_types"][idx]
+
+                if "allow_left" in data:
+                    child_class.value = data["name"]
+                    if "display_name" in data:
+                        child_class.label = data["display_name"]
+                    self.dom_elements["select_weapon_type_left"].appendChild(child_class)
+
 
         # initilzed finish
         self._initialized = True
@@ -120,15 +226,15 @@ class Simulator:
                 self.dom_elements["job_lv"].value = data_dict["status"]["job_lv"]
 
             if "job_class_id" in data_dict["status"]:
-                self.dom_elements["job_class"].value = self.data_job_classes[str(data_dict["status"]["job_class_id"])]["class"]
+                self.dom_elements["job_class"].value = self.load_datas["job_classes"][str(data_dict["status"]["job_class_id"])]["class"]
 
-            for key in self._status_basic:
+            for key in self._status_bases.keys():
                 if key in data_dict["status"]:
-                    self.dom_elements[key]["basic"].value = data_dict["status"][key]
+                    self.dom_elements[key]["base"].value = data_dict["status"][key]
 
-            for key in self._status_special:
+            for key in self._status_specials.keys():
                 if key in data_dict["status"] and len(self.dom_elements[key]) > 0:
-                    self.dom_elements[key]["basic"].value = data_dict["status"][key]
+                    self.dom_elements[key]["base"].value = data_dict["status"][key]
 
         if "skills" in data_dict:
             pass
@@ -170,20 +276,20 @@ class Simulator:
             }
         }
 
-        for key in self._status_basic:
+        for key in self._status_bases.keys():
             value: int = 0
             try:
-                value = int(self.dom_elements[key]["basic"].value)
+                value = int(self.dom_elements[key]["base"].value)
             except ValueError:
                 pass
 
             data_json["status"][key] = value
 
-        for key in self._status_special:
+        for key in self._status_specials:
             if key in self.dom_elements and len(self.dom_elements[key]) > 0:
                 value: int = 0
                 try:
-                    value = int(self.dom_elements[key]["basic"].value)
+                    value = int(self.dom_elements[key]["base"].value)
                 except ValueError:
                     pass
 
@@ -192,7 +298,7 @@ class Simulator:
 
         job_class = self.dom_elements["job_class"].value
         if job_class is not None and job_class != "":
-            job_class_ids = [key for key, value in self.data_job_classes.items() if value["class"] == job_class]
+            job_class_ids = [key for key, value in self.load_datas["job_classes"].items() if value["class"] == job_class]
             if len(job_class_ids) > 0:
                 data_json["status"]["job_class_id"] = int(job_class_ids[0])
 
@@ -220,7 +326,7 @@ class Simulator:
 
         try:
             # calculation
-            package.module_v1.calc(self.dom_elements)
+            package.module_v1.pre_calc(self.dom_elements, self.load_datas)
 
         except Exception as ex:
             success = False
@@ -228,6 +334,39 @@ class Simulator:
 
         if success == True:
             self.export_to_base64()
+
+    def onclick_draw_status_window(self, event = None):
+        self.draw_img_status_window()
+
+    def draw_img_status_window(self, img_src: str = "./assets/statwin_bg.bmp"):
+        img = Image.open(img_src)
+        font = ImageFont.truetype("./assets/SourceCodePro-Regular.ttf", 10, 0)
+        draw = ImageDraw.Draw(img)
+
+        for key in self._status_bases.keys():
+            text = self.dom_elements[key]["base"].value
+            text += "+"
+            text += self.dom_elements[key]["bonus"].value
+            position = self._status_bases[key]["status_window_position"]
+            draw.text(position, text, "#000000", font=font, align="left")
+
+        for key in self._status_specials.keys():
+            text = self.dom_elements[key]["base"].value
+            text += "+"
+            text += self.dom_elements[key]["bonus"].value
+            position = self._status_specials[key]["status_window_position"]
+            draw.text(position, text, "#000000", font=font, align="left")
+
+        # zoom x2
+        img = img.resize((img.width * 2, img.height * 2), Image.NEAREST)
+
+        buffer = BytesIO()
+        img.save(buffer, "png")
+        img_str = "data:image/bmp;base64," + base64.b64encode(buffer.getvalue()).decode("ascii")
+        img_status_window = self.dom_elements["img_status_window"]
+        img_status_window.src = img_str
+        img_status_window.width = img.width
+        img_status_window.height = img.height
 
     def view_dialog(self, message: str = "") -> None:
         message_div = document.getElementById("dialog_information_message_div")
@@ -259,7 +398,7 @@ def main():
             return
 
     instance.calculation()
-    instance.export_to_base64()
+    instance.draw_img_status_window()
 
 if __name__ == "__main__":
     main()
